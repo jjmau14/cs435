f8e5bea76e1a541f42811d36dcb8d33973515628
jjmau14 <jjmau14@gmail.com>
2018-09-17 16:56:25 -0600

    wef

--- pa1/src/main/java/cs435/pa1/PA1.java
+++ pa1/src/main/java/cs435/pa1/PA1.java
@@ -2,6 +2,7 @@ package cs435.pa1;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -23,6 +24,9 @@ public class PA1 {
         job.setReducerClass(Profile1Reducer.class);
 
         job.setNumReduceTasks(3);
+
+        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputValueClass(Text.class);
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
